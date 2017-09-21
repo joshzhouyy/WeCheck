1f8981d4a40776aec15699964f68eff511999072
maoxia <luo149@purdue.edu>
2017-09-21 09:34:38 -0400

    change folder public to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+const BUILD_DIR = path.resolve(__dirname, 'src/client/dist');
 const APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
