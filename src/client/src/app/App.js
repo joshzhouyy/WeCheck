7fbc8d7197326b1b5ec3195df204314c9fab6acc
Moon sun Hwang <moonsunhwang@pal-nat186-102-123.itap.purdue.edu>
2017-10-12 15:36:38 -0400

    added event panels

--- src/client/src/app/App.js
+++ src/client/src/app/App.js
@@ -2,14 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Subheader from './Subheader'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import EventMemberPanel from '../event/EventMemberPanel'
 
+
+const muiTheme = getMuiTheme({
+  fontFamily: 'Alegreya Sans SC'
+  
+  
+});
+
+
+const Panel = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <EventMemberPanel />
+  </MuiThemeProvider>
+);
 
 class App extends React.Component {
 
 	render(){
 
 		return (
-			<div><Subheader headerName='Dashboard' /></div>
+			<Panel />
 		);
 	}
 }
