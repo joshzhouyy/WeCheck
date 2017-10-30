20bad4eec9195ac74699e81e670ce6c592c6eba4
eric xx <ericxx@pal-nat186-20-153.itap.purdue.edu>
2017-10-30 00:34:58 -0400

    #25

--- src/client/src/utilities/Topbar.js
+++ src/client/src/utilities/Topbar.js
@@ -1,3 +1,53 @@
+// working code
+// import React from 'react';
+// import AppBar from 'material-ui/AppBar';
+// import FlatButton from 'material-ui/FlatButton';
+// import Drawer from 'material-ui/Drawer';
+// import MenuItem from 'material-ui/MenuItem';
+// import RaisedButton from 'material-ui/RaisedButton';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+// import App from '../app/App';
+// import WelcomePage from '../event/WelcomePage';
+
+
+
+
+// const styles = {
+//   title: {
+//     cursor: 'pointer',
+//     postion: 'relative'
+//   },
+//   buttonStyle: {
+//     backgroundColor: 'transparent',
+//     color: 'white',
+//     fontSize: '1em',
+//     height:'2em',
+//   },
+// };
+
+// const rightButtons = (onClick) => (
+//     <div>
+//       <FlatButton label="Sign Out"  style={styles.buttonStyle} onClick={() => onClick()}/>
+//     </div>
+//   );
+
+
+
+// const Topbar = ({onClick}) => (
+//   <MuiThemeProvider>
+//     <div id="topBar">
+//       <AppBar
+//         title={<span style={styles.title}>WeCheck</span>}
+//         iconElementRight={rightButtons(onClick)}
+//       />
+//     </div>
+//   </MuiThemeProvider>
+// );
+
+// export default Topbar;
+
+// buggy code #25
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
@@ -27,7 +77,7 @@ const styles = {
 
 const rightButtons = (onClick) => (
     <div>
-      <FlatButton label="Sign Out"  style={styles.buttonStyle} onClick={() => onClick()}/>
+      <FlatButton label=""  style={styles.buttonStyle} onClick={() => onClick()}/>
     </div>
   );
 
