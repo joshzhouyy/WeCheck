8e7ce8fbd326a2f485eb4d520070e80638d06110
maoxia <luo149@purdue.edu>
2017-10-31 15:48:45 -0400

    Manually added backend updates

--- src/backend_src/models/user.js
+++ src/backend_src/models/user.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new mongoose.Schema({
-   	userName: String,
+    userName: String,
     userAccount: {type: String, unique: true},
     password: String,
     //list that contains all event that a user owns or in
-    eventList:[{ type: Schema.Types.ObjectId, ref: 'eventinfo'}]
+    eventList:[{ type: Schema.Types.ObjectId, ref: 'eventinfo'}],
+    pendingInvites:[{type: Schema.Types.ObjectId, ref: 'eveninfo'}]
 });
 
 module.exports = mongoose.model('user', userSchema);
